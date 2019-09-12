02e9569c2a199eae2d68bd320629576d17f0d322
diegopgcabral <diegopgc@gmail.com>
2019-09-11 23:48:08 -0300

    Feature: Configurando Fila com Redis

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,35 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    // Chave única, como o mesmo nome da classe, cada JOB precisa de uma chave.
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A Fila executou.');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
